2e1db9298184cbfb58349285a4e48fd5015d2fca
alexandraruth <66081217+alexandraruth@users.noreply.github.com>
2021-09-20 20:01:08 -0400

    zebra count updated 9-20

--- zebraplot.R
+++ zebraplot.R
@@ -2,6 +2,15 @@
 
 # load packages
 library(tidyverse)
+library(png)
+library(ggimage)
+
+# Timeline of reported zebra counts
+
+# August 31: citizen reports seeing a zebra dash in front of a car (https://www.fox5dc.com/news/maryland-man-says-he-spotted-runaway-zebras-outside-upper-marlboro-home)
+# September 7: WTOP reports a dazzle of three zebras on the loose (https://wtop.com/prince-georges-county/2021/09/they-will-run-you-over-prince-georges-co-residents-warned-to-steer-clear-of-escaped-zebras/)
+# September 9: WaPo & NYT report five zebras (https://www.nytimes.com/2021/09/09/us/zebras-maryland.html) (https://www.washingtonpost.com/dc-md-va/2021/09/08/zebras-escape-maryland-farm/)
+# September 10: NBC reports six zebras; Eleanor Holmes Norton denies involvement (https://www.washingtonpost.com/dc-md-va/2021/09/08/zebras-escape-maryland-farm/)
 
 
 # Create dataframe
@@ -10,24 +19,41 @@ date <- seq(as.Date('2021-08-28'), as.Date('2021-09-21'), by = 'days')
 
 zebras <- data.frame(date) %>%
   mutate(count = NA) %>%
-  mutate(count = ifelse(date < '2021-08-31', 0, count)) %>% # source: 
-  mutate(count = ifelse(date >= '2021-08-31' & date < '2021-09-07', 1, count)) # source: 
+  mutate(count = ifelse(date < '2021-08-31', 0, count)) %>% 
+  mutate(count = ifelse(date >= '2021-08-31' & date <= '2021-09-06', 1, count)) %>% 
+  mutate(count = ifelse(date == '2021-09-07' | date == '2021-09-08', 3, count)) %>% 
+  mutate(count = ifelse(date == '2021-09-09', 5, count)) %>% 
+  mutate(count = ifelse(date >= '2021-09-10' & date < '2021-09-21', 6, count))  
+
+img <- readPNG("zebra2.png", native=TRUE)
+
+zebras <- zebras %>%
+  mutate(IMAGE = ifelse(date== '2021-08-30' | date =='2021-08-31' | date == '2021-09-07' | date == '2021-09-09' | date == '2021-09-10' | date == '2021-09-09', "zebra.png", NA))
+  
+# simple plot
+
+p <- ggplot(zebras, aes(x=date, y=count)) + 
+  geom_line() +
+  labs(title = "Changes in Reported Count of Escaped Zebras in Maryland (Sept. 2021)", x = "Date", y = "Reported zebras at large") +
+  scale_y_continuous(limits=c(0,6), breaks = seq(0, 6, by = 1)) +
+  scale_x_date(date_labels = "%b %d", breaks = "2 days", expand=c(0,0)) +
+  geom_image(aes(image = IMAGE), size = 0.07) +
+  theme(
+    panel.background = element_rect(fill = "lightblue",
+                                    colour = "lightblue",
+                                    size = 0.3, linetype = "solid"),
+    panel.grid.major = element_line(size = 0.3, linetype = 'solid',
+                                    colour = "white"), 
+    panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                    colour = "white")
+  ) + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+p  
 
 
-
-# First zebra sighted and reported August 31
-
-
-# Reported zebra count
-
-
-# Mixed reporting on zebra counts; six count reported in several outlets
-
-# Create count plot of zebras
-
-ggplot()
-
+ 
 # insert zebra emojis
 
 
-# insert additional plot zebras
+
+# insert additional plot details
