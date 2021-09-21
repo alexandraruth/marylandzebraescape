1b58396e5850d8be0808a6723f3440194c1b382b
alexandraruth <66081217+alexandraruth@users.noreply.github.com>
2021-09-20 20:55:16 -0400

    updated plot labels

--- zebraplot.R
+++ zebraplot.R
@@ -4,8 +4,9 @@
 library(tidyverse)
 library(png)
 library(ggimage)
+library(ggrepel)
 
-# Timeline of reported zebra counts
+# Timeline of reported zebra counts ----
 
 # August 31: citizen reports seeing a zebra dash in front of a car (https://www.fox5dc.com/news/maryland-man-says-he-spotted-runaway-zebras-outside-upper-marlboro-home)
 # September 7: WTOP reports a dazzle of three zebras on the loose (https://wtop.com/prince-georges-county/2021/09/they-will-run-you-over-prince-georges-co-residents-warned-to-steer-clear-of-escaped-zebras/)
@@ -13,7 +14,7 @@ library(ggimage)
 # September 10: NBC reports six zebras; Eleanor Holmes Norton denies involvement (https://www.washingtonpost.com/dc-md-va/2021/09/08/zebras-escape-maryland-farm/)
 
 
-# Create dataframe
+# Create dataframe ----
 
 date <- seq(as.Date('2021-08-28'), as.Date('2021-09-21'), by = 'days')
 
@@ -30,7 +31,7 @@ img <- readPNG("zebra2.png", native=TRUE)
 zebras <- zebras %>%
   mutate(IMAGE = ifelse(date== '2021-08-30' | date =='2021-08-31' | date == '2021-09-07' | date == '2021-09-09' | date == '2021-09-10' | date == '2021-09-09', "zebra.png", NA))
   
-# simple plot
+# simple plot ----
 
 p <- ggplot(zebras, aes(x=date, y=count)) + 
   geom_line() +
@@ -47,13 +48,22 @@ p <- ggplot(zebras, aes(x=date, y=count)) +
     panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
                                     colour = "white")
   ) + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-p  
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  annotate(
+    "text", label = "Tripwire trap",
+    x = as.Date('2021-09-11'), y = 4.65, size = 3, colour = "black"
+  ) +
+  annotate(
+    "text", label = "construction begins",
+    x = as.Date('2021-09-11'), y = 4.35, size = 3, colour = "black"
+  ) +
+  annotate(
+    "text", label = "6 zebras still at large",
+    x = as.Date('2021-09-18'), y = 5.75, size = 3, colour = "black"
+  )
+
+p
+
 
 
  
-# insert zebra emojis
-
-
-
-# insert additional plot details
